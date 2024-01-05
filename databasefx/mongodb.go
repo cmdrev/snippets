b5318023fa846904f6466e9a4d9edbefb6304e5c
CmdRev <cmdrev@posteo.de>
2024-01-05 14:24:19 +0100

    add default database selection for mongodb

--- databasefx/mongodb.go
+++ databasefx/mongodb.go
@@ -2,13 +2,15 @@ package databasefx
 
 import (
 	"context"
+	"fmt"
 	"github.com/cmdrev/snippets/config"
 	"github.com/cmdrev/snippets/database"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/connstring"
 	"go.uber.org/fx"
 )
 
-func NewMongoDBSession(lc fx.Lifecycle, config database.MongoDBConfig) (*mongo.Client, error) {
+func NewMongoDBClient(lc fx.Lifecycle, config database.MongoDBConfig) (*mongo.Client, error) {
 	client, err := database.NewMongoDbSession(context.TODO(), config)
 	if err != nil {
 		return nil, err
@@ -21,7 +23,16 @@ func NewMongoDBSession(lc fx.Lifecycle, config database.MongoDBConfig) (*mongo.C
 	return client, nil
 }
 
-var MongoDBModule = fx.Module("mongodb-session",
+func MongoClientWithDatabase(client *mongo.Client, config database.MongoDBConfig) (*mongo.Database, error) {
+	cs, err := connstring.ParseAndValidate(config.ConnectionURI())
+	if err != nil {
+		return nil, fmt.Errorf("invalid mongodb connection uri: %w", err)
+	}
+	return client.Database(cs.Database), nil
+}
+
+var MongoDBModule = fx.Module("mongodb",
 	fx.Provide(config.NewMongoDBConfig),
-	fx.Provide(NewMongoDBSession),
+	fx.Provide(NewMongoDBClient),
+	fx.Provide(MongoClientWithDatabase),
 )
